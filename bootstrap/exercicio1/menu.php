0a487fc0c06936e0496bd898d6bcb992f86866b6
iespenanovo <correo.iaw@gmail.com>
2021-04-27 11:13:51 +0200

    html asir1

--- bootstrap/exercicio1/menu.php
+++ bootstrap/exercicio1/menu.php
@@ -4,31 +4,31 @@
 		<div class="row">
 			<div class="col-sm-4 col-md-2">
 				<a href="index.php">
-					<img src="img/logo.png" class="img-responsive" width="200">
+					<img src="img/logo.png" class="img-fluid" width="200">
 				</a>
 			</div>
 			<div class="col-md-5 col-sm-8">
-				<div class="btn-group btn-group-justified" role="group" aria-label="...">
-					<a href="index.php" class="btn btn-info">Inicio</a>
+				<div class="btn-group w-100" role="group" aria-label="...">
+					<a href="index.php" class="btn btn-light">Inicio</a>
 					<a href="" class="btn btn-primary" data-toggle="modal" data-target="#modalCursos">
 						Cursos
 					</a>
 				</div>
 			</div>
 
-			<div class="col-md-5 col-md-offset-0 col-sm-8 col-sm-offset-4 ">
-				<div class="btn-group btn-group-justified" role="group" aria-label="...">
-					<a href="contacto.php" class="btn btn-warning">Contacto</a>
-					<div class="btn-group" role="group">
-						<button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+			<div class="col-md-5 offset-md-0 col-sm-8 offset-sm-4 ">
+				<div class="btn-group w-100" role="group" aria-label="...">
+					<a href="contacto.php" class="btn btn-warning" >Contacto</a>
+					<div class="btn-group" role="group" >
+						<button id="btn1" type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
 							Categorías
-							<span class="caret"></span>
+							<!-- <span class="caret"></span> -->
 						</button>
-						<ul class="dropdown-menu" role="menu">
-							<li><a href="#">Categoría A</a></li>
-							<li><a href="#">Categoría B</a></li>
-							<li class="divider"></li>
-							<li><a href="#">Categoría C</a></li>
+						<ul class="dropdown-menu" aria-labelledby="btn1">
+							<li><a class="dropdown-item" href="#">Categoría A</a></li>
+							<li><a class="dropdown-item" href="#">Categoría B</a></li>
+							<!-- <li class="divider"></li> -->
+							<li><a class="dropdown-item" href="#">Categoría C</a></li>
 						</ul>
 					</div>
 
