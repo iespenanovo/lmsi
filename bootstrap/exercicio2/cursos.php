8c8868c7863f14d1736029f45f81d023458948bd
iespenanovo <correo.iaw@gmail.com>
2021-05-05 09:35:18 +0200

    html asir1

--- bootstrap/exercicio2/cursos.php
+++ bootstrap/exercicio2/cursos.php
@@ -7,22 +7,162 @@
 
 
   <div id="cursos" >
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-6">
+         <!-- El siguiente código emmet genera la tabla A de cursos:-->
+         <!-- table>caption{Cursos A}+(tr>th{Nº}+th{Código}+th{Denominación}+th{Nº alumnos})+(tr>td{Nº $}+td{Código AXRT$}+td{Denominación Ciclo $}+td{0$})*10 -->
+         <table class="table table-responsive table-striped text-center">
+           <caption class="bg-danger">Cursos A</caption>
+           <tr class="bg-danger">
+             <th>Nº</th>
+             <th>Código</th>
+             <th>Denominación</th>
+             <th>Nº alumnos</th>
+           </tr>
+           <tr>
+             <td>Nº 1</td>
+             <td>Código AXRT1</td>
+             <td>Denominación Ciclo 1</td>
+             <td>01</td>
+           </tr>
+           <tr>
+             <td>Nº 2</td>
+             <td>Código AXRT2</td>
+             <td>Denominación Ciclo 2</td>
+             <td>02</td>
+           </tr>
+           <tr>
+             <td>Nº 3</td>
+             <td>Código AXRT3</td>
+             <td>Denominación Ciclo 3</td>
+             <td>03</td>
+           </tr>
+           <tr>
+             <td>Nº 4</td>
+             <td>Código AXRT4</td>
+             <td>Denominación Ciclo 4</td>
+             <td>04</td>
+           </tr>
+           <tr>
+             <td>Nº 5</td>
+             <td>Código AXRT5</td>
+             <td>Denominación Ciclo 5</td>
+             <td>05</td>
+           </tr>
+           <tr>
+             <td>Nº 6</td>
+             <td>Código AXRT6</td>
+             <td>Denominación Ciclo 6</td>
+             <td>06</td>
+           </tr>
+           <tr>
+             <td>Nº 7</td>
+             <td>Código AXRT7</td>
+             <td>Denominación Ciclo 7</td>
+             <td>07</td>
+           </tr>
+           <tr>
+             <td>Nº 8</td>
+             <td>Código AXRT8</td>
+             <td>Denominación Ciclo 8</td>
+             <td>08</td>
+           </tr>
+           <tr>
+             <td>Nº 9</td>
+             <td>Código AXRT9</td>
+             <td>Denominación Ciclo 9</td>
+             <td>09</td>
+           </tr>
+           <tr>
+             <td>Nº 10</td>
+             <td>Código AXRT10</td>
+             <td>Denominación Ciclo 10</td>
+             <td>010</td>
+           </tr>
+         </table>
+       </div>
+
+       <div class="col-md-6">
+         <!-- El siguiente código emmet genera la tabla B de cursos:-->
+         <!-- table>caption{Cursos B}+(tr>th{Nº}+th{Código}+th{Denominación}+th{Nº alumnos})+(tr>td{Nº $}+td{Código BXRT$}+td{Denominación Ciclo $}+td{0$})*10 -->
+         <table class="table table-responsive table-striped text-center">
+           <caption class="bg-info">Cursos B</caption>
+           <tr class="bg-info">
+             <th>Nº</th>
+             <th>Código</th>
+             <th>Denominación</th>
+             <th>Nº alumnos</th>
+           </tr>
+           <tr>
+             <td>Nº 1</td>
+             <td>Código BXRT1</td>
+             <td>Denominación Ciclo 1</td>
+             <td>01</td>
+           </tr>
+           <tr>
+             <td>Nº 2</td>
+             <td>Código BXRT2</td>
+             <td>Denominación Ciclo 2</td>
+             <td>02</td>
+           </tr>
+           <tr>
+             <td>Nº 3</td>
+             <td>Código BXRT3</td>
+             <td>Denominación Ciclo 3</td>
+             <td>03</td>
+           </tr>
+           <tr>
+             <td>Nº 4</td>
+             <td>Código BXRT4</td>
+             <td>Denominación Ciclo 4</td>
+             <td>04</td>
+           </tr>
+           <tr>
+             <td>Nº 5</td>
+             <td>Código BXRT5</td>
+             <td>Denominación Ciclo 5</td>
+             <td>05</td>
+           </tr>
+           <tr>
+             <td>Nº 6</td>
+             <td>Código BXRT6</td>
+             <td>Denominación Ciclo 6</td>
+             <td>06</td>
+           </tr>
+           <tr>
+             <td>Nº 7</td>
+             <td>Código BXRT7</td>
+             <td>Denominación Ciclo 7</td>
+             <td>07</td>
+           </tr>
+           <tr>
+             <td>Nº 8</td>
+             <td>Código BXRT8</td>
+             <td>Denominación Ciclo 8</td>
+             <td>08</td>
+           </tr>
+           <tr>
+             <td>Nº 9</td>
+             <td>Código BXRT9</td>
+             <td>Denominación Ciclo 9</td>
+             <td>09</td>
+           </tr>
+           <tr>
+             <td>Nº 10</td>
+             <td>Código BXRT10</td>
+             <td>Denominación Ciclo 10</td>
+             <td>010</td>
+           </tr>
+         </table>
+       </div>
+     </div>
+   </div>
+ </div>
 
 
-  	<!-- zona menu, debes borrar la etiqueta h1 siguiente:-->
-	<h1 style="margin:0;height: 630px; padding-top: 50px; color:#fff" class="text-center bg-success">Zona cursos</h1>
-
-  
-	<!-- El siguiente código emmet genera la tabla A de cursos:-->
-    <!-- table>caption{Cursos A}+(tr>th{Nº}+th{Código}+th{Denominación}+th{Nº alumnos})+(tr>td{Nº $}+td{Código AXRT$}+td{Denominación Ciclo $}+td{0$})*10 -->
-       
-	<!-- El siguiente código emmet genera la tabla B de cursos:-->
-    <!-- table>caption{Cursos B}+(tr>th{Nº}+th{Código}+th{Denominación}+th{Nº alumnos})+(tr>td{Nº $}+td{Código BXRT$}+td{Denominación Ciclo $}+td{0$})*10 -->
-
-  </div>
-
-
-  <?php include("pie.php") ?>  
+ <?php include("pie.php") ?>  
 
 </body>
 </html>
