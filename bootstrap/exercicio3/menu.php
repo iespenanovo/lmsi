ebfa3c2c66769f2f138ec1096c56afe407c98658
iespenanovo <correo.iaw@gmail.com>
2021-05-12 09:36:21 +0200

    html asir1

--- bootstrap/exercicio3/menu.php
+++ bootstrap/exercicio3/menu.php
@@ -1,3 +1,27 @@
 <div id="menu">
-	<h1>Menú</h1>
+	<div class="container">	
+		<div class="row">
+			<div class="">
+				<a href="index.php">
+					<img src="img/logo.png" class="img-fluid" alt="Titania">
+				</a>
+			</div>
+			<div class="">
+				<div class="btn-group d-flex" role="group" aria-label="Barra 1">
+					<a href="index.php" class="btn btn-light w-100">Inicio</a>
+					<a href="quensomos.php" class="btn btn-light w-100">Quen somos?</a>
+					<a href="servicios.php" class="btn btn-light w-100">Servicios</a>
+				</div>					
+			</div>
+			<div class="">
+				<div class="btn-group d-flex" role="group" aria-label="Barra 2">
+					<a href="blog.php" class="btn btn-light w-100">Blog</a>
+					<a class="btn btn-light w-100" data-bs-toggle="modal" data-bs-target="#contacto">Contacto</a>
+					<a class="btn btn-light w-100" data-bs-toggle="modal" data-bs-target="#axuda">Axuda</a>
+				</div>					
+
+			</div>
+		</div>
+	</div>
+
 </div>
