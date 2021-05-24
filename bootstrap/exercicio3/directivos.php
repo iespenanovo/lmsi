962189f408fadf428bd9f863ac540fd3502a07a4
iespenanovo <correo.iaw@gmail.com>
2021-05-24 17:21:55 +0200

    html asir1

--- bootstrap/exercicio3/directivos.php
+++ bootstrap/exercicio3/directivos.php
@@ -8,29 +8,29 @@
 				<h5>Director general</h5>
 			</div>
 			<div class="col-6 col-md-4 col-lg-2">
-				<img src="img/cargo1.jpg" class="img-fluid" >
-				<h3>Pedro López</h3>
-				<h5>Director general</h5>
+				<img src="img/cargo2.jpg" class="img-fluid" >
+				<h3>Elena Nito</h3>
+				<h5>Subdirectora</h5>
 			</div>
 			<div class="col-6 col-md-4 col-lg-2">
-				<img src="img/cargo1.jpg" class="img-fluid" >
-				<h3>Pedro López</h3>
-				<h5>Director general</h5>
+				<img src="img/cargo3.jpg" class="img-fluid" >
+				<h3>Aitor Menta</h3>
+				<h5>Jefe ventas</h5>
 			</div>
 			<div class="col-6 col-md-4 col-lg-2">
-				<img src="img/cargo1.jpg" class="img-fluid" >
-				<h3>Pedro López</h3>
-				<h5>Director general</h5>
+				<img src="img/cargo4.jpg" class="img-fluid" >
+				<h3>Paula Pérez</h3>
+				<h5>Programadora</h5>
 			</div>
 			<div class="col-6 col-md-4 col-lg-2">
-				<img src="img/cargo1.jpg" class="img-fluid" >
-				<h3>Pedro López</h3>
-				<h5>Director general</h5>
+				<img src="img/cargo5.jpg" class="img-fluid" >
+				<h3>Pedro Lores</h3>
+				<h5>Recursos humanos</h5>
 			</div>
 			<div class="col-6 col-md-4 col-lg-2">
-				<img src="img/cargo1.jpg" class="img-fluid" >
-				<h3>Pedro López</h3>
-				<h5>Director general</h5>
+				<img src="img/cargo6.jpg" class="img-fluid" >
+				<h3>Sara Gosa</h3>
+				<h5>Supervisor</h5>
 			</div>
 		</div>
 	</div>
--- bootstrap/exercicio3/pe.php
+++ bootstrap/exercicio3/pe.php
@@ -1,4 +1,55 @@
 <div id="pe">
-	<h1>Pe</h1>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-lg-3">
+				<h3>Sobre nós</h3>
+				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias ipsum eveniet, quis ratione suscipit blanditiis at voluptatibus magni pariatur modi.</p>
+			</div>
+			<div class="col-md-6 col-lg-3">
+				<h3>Últimas noticias</h3>
+				<ul class="list-unstyled">
+					<li><a href="#">Noticia 1</a></li>
+					<li><a href="#">Noticia 2</a></li>
+					<li><a href="#">Noticia 3</a></li>
+					<li><a href="#">Noticia 4</a></li>
+				</ul>
+				
+			</div>
+			<div class="col-md-6 col-lg-3">
+				<h3>Contacto</h3>
+				<ul class="list-unstyled">
+					<li><i class="fi-address-book"></i> Rúa Terra Cha, 33</li>
+					<li><i class="fi-telephone"></i> 982 99 99 99</li>
+					<li><i class="fi-mail"></i> info@titania.com</li>
+					<li><i class="fi-web"></i> www.titania.com</li>
+				</ul>
+			</div>
+			<div class="col-md-6 col-lg-3">
+				<h3>Enlaces de interés</h3>
+				<ul class="list-unstyled">
+					<li><a href="https://es.wordpress.org/" target="_blank">Wordpress</a></li>
+					<li><a href="http://iespenanovo.com" target="_blank">IES Lois Peña Novo</a></li>
+					<li><a href="http://edu.xunta.gal" target="_blank">Consellería de educación</a></li>
+					<li><a href="http://getbootstrap.com" target="_blank">Bootstrap framework</a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="peFila2" class="row text-center">
+			<div class="col-lg-3">
+				<a href="#">Política de privacidade</a>
+			</div>
+			<div class="col-lg-6">
+				<p>Copyright © 2021. Todos os dereitos reservados</p>
+			</div>
+			<div class="col-lg-3">
+				<a href="#"><i class="fi-social-facebook"></i></a> 
+				<a href="#"><i class="fi-social-twitter"></i></a> 
+				<a href="#"><i class="fi-social-linkedin"></i></a> 
+				<a href="#"><i class="fi-social-google-plus"></i></a> 
+				<a href="#"><i class="fi-social-pinterest"></i></a>
+			</div>
+		</div>
+
+	</div>
 </div>
 
