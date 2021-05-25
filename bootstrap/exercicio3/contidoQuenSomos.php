ac03eaf28b58bc5dd3ce8909a086e8644c7a2f13
iespenanovo <correo.iaw@gmail.com>
2021-05-25 11:14:32 +0200

    html asir1

--- bootstrap/exercicio3/contidoQuenSomos.php
+++ bootstrap/exercicio3/contidoQuenSomos.php
@@ -1,10 +1,14 @@
 <div id="quenSomos" class="container">
 	<div class="row">
 		<div class="col-md-6">
-			
+			<h3>Quen somos?</h3>
+			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore vel fugit, vero recusandae omnis laudantium aperiam aliquid dolorem, numquam iusto officia, quos eius sunt quia quam rerum culpa. Reiciendis earum in eius aspernatur consequatur magnam, excepturi, fugiat esse quasi tempora, corrupti, neque! Officia molestias, soluta doloremque excepturi, ipsam quae ex ipsa sequi, nemo magnam iusto suscipit eius praesentium distinctio adipisci quis numquam nobis consequuntur iste. Cumque sint alias, sapiente possimus, hic dicta deserunt soluta aspernatur quibusdam quia nobis in deleniti quos quae perferendis, fugit tempore? Magni quo, porro repudiandae fugit consectetur reprehenderit, dignissimos dicta iusto odio modi aliquid doloremque pariatur?</p>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Perferendis quod sit rerum pariatur doloribus, unde natus illo eos accusamus porro quas animi aspernatur vitae ratione facilis, est amet perspiciatis rem. Dicta a deleniti, iure minima ullam obcaecati numquam illum quae nam placeat ab culpa sequi natus praesentium ducimus commodi quibusdam laudantium, explicabo amet molestiae repellendus. Dolores harum accusamus autem pariatur exercitationem magni, veritatis ab necessitatibus quibusdam inventore assumenda saepe et esse neque debitis officiis similique blanditiis temporibus labore. Vitae doloremque explicabo beatae dolorum et aperiam, nulla, a facilis, sit accusamus magni odio aspernatur quia consectetur harum eum laboriosam recusandae repudiandae!</p>
 		</div>
+
 		<div class="col-md-6">
-			
+			<h3>Localización</h3>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2903.875389838652!2d-7.678953084230748!3d43.29593118347676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd31e6a27d718041%3A0x4908a6183694aa92!2sAv.%20da%20Terra%20Cha%2C%2033%2C%2027800%20Villalba%2C%20Lugo!5e0!3m2!1ses!2ses!4v1621931834559!5m2!1ses!2ses" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 		</div>
 	</div>
 </div>
---
+++ bootstrap/exercicio3/contidoServicios.php
@@ -0,0 +1,3 @@
+<div id="servicios" class="container">
+	
+</div>
