f33155dab366755e2e606dbf897eb1e28146d41e
iespenanovo <correo.iaw@gmail.com>
2021-05-26 09:19:54 +0200

    html asir1

---
+++ bootstrap/exercicio3/blog.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Quen somos - Titania</title>
+	<!-- na seguinte instrucción php incluimos chamada a librerías css de bootstrap, de iconos, e a noso css propio	 -->
+	<?php include("css.php");//librerías css de bootstrap e outras  ?>
+</head>
+<body>
+	<?php 
+		include("menu.php");
+
+		include("contidoBlog.php");
+
+		include("pe.php");
+		include("javascript.php"); //Librerías javascript de bootstrap para compoñentes
+	?>
+	
+</body>
+</html>
--- bootstrap/exercicio3/contidoServicios.php
+++ bootstrap/exercicio3/contidoServicios.php
@@ -1,3 +1,41 @@
-<div id="servicios" class="container">
-	
+<div id="servicios" class="container text-center">
+	<div class="row">
+		<div class="col-md-6 col-lg-4">
+			<i class="fi-camera"></i>
+			<h3>Servicio 1</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur, adipisicing elit. Voluptatum aliquid tempore similique culpa necessitatibus nesciunt! Omnis ducimus dolorem quam corrupti fuga blanditiis numquam fugit praesentium deleniti est, totam voluptatibus ipsam.</p>
+			<a href="#" class="btn btn-primary">Máis Información</a>
+		</div>
+		<div class="col-md-6 col-lg-4">
+			<i class="fi-cloud"></i>
+			<h3>Servicio 2</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur, adipisicing elit. Voluptatum aliquid tempore similique culpa necessitatibus nesciunt! Omnis ducimus dolorem quam corrupti fuga blanditiis numquam fugit praesentium deleniti est, totam voluptatibus ipsam.</p>
+			<a href="#" class="btn btn-primary">Máis Información</a>
+		</div>
+		<div class="col-md-6 col-lg-4">
+			<i class="fi-universal-access"></i>
+			<h3>Servicio 3</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur, adipisicing elit. Voluptatum aliquid tempore similique culpa necessitatibus nesciunt! Omnis ducimus dolorem quam corrupti fuga blanditiis numquam fugit praesentium deleniti est, totam voluptatibus ipsam.</p>
+			<a href="#" class="btn btn-primary">Máis Información</a>
+		</div>
+		<div class="col-md-6 col-lg-4">
+			<i class="fi-camera"></i>
+			<h3>Servicio 4</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur, adipisicing elit. Voluptatum aliquid tempore similique culpa necessitatibus nesciunt! Omnis ducimus dolorem quam corrupti fuga blanditiis numquam fugit praesentium deleniti est, totam voluptatibus ipsam.</p>
+			<a href="#" class="btn btn-primary">Máis Información</a>
+		</div>
+		<div class="col-md-6 col-lg-4">
+			<i class="fi-cloud"></i>
+			<h3>Servicio 5</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur, adipisicing elit. Voluptatum aliquid tempore similique culpa necessitatibus nesciunt! Omnis ducimus dolorem quam corrupti fuga blanditiis numquam fugit praesentium deleniti est, totam voluptatibus ipsam.</p>
+			<a href="#" class="btn btn-primary">Máis Información</a>
+		</div>
+		<div class="col-md-6 col-lg-4">
+			<i class="fi-universal-access"></i>
+			<h3>Servicio 6</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur, adipisicing elit. Voluptatum aliquid tempore similique culpa necessitatibus nesciunt! Omnis ducimus dolorem quam corrupti fuga blanditiis numquam fugit praesentium deleniti est, totam voluptatibus ipsam.</p>
+			<a href="#" class="btn btn-primary">Máis Información</a>
+		</div>
+
+	</div>
 </div>
