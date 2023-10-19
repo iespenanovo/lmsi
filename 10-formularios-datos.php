18fb11505351f0479279a1a35f845c724bf7b099
iespenanovo <correo.iaw@gmail.com>
2023-10-19 15:53:29 +0200

    ASIR1 LMSI

---
+++ 10-formularios-datos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Datos recibidos del formulario</title>
+</head>
+<body>
+	<h1>Datos recibidos del formulario 10-formularios.html :</h1>
+
+	<?php //aquí va código PHP, para generación de HTML dinámico:
+
+	$nombre=$_GET['nombre']??"";
+	$clave=$_GET['clave']??"";
+
+
+	echo "<p>Nombre: $nombre</p>";
+	echo "<p>Contraseña: $clave</p>";
+
+
+	?>
+
+</body>
+</html>
